3252ebf3025f482b0308d08387ac9ccfe3b4b7e7
liangluping <348963373@qq.com>
2024-08-21 16:42:20 +0800

    note-detail

--- src/pages/tagging/note-detail.vue
+++ src/pages/tagging/note-detail.vue
@@ -5,7 +5,111 @@
 </style>
 <template>
   <section style="display: flex;">
-    222
+    <section className="note-item" data-width="1083" data-height="1443" data-index="0">
+      <div data-v-e43676be=""><a data-v-e43676be="" href="/explore/66c32430000000001d0149d5" style="display: none;"></a><a
+          data-v-e43676be="" className="cover ld mask" target="_self"
+          href="/explore/66c32430000000001d0149d5?xsec_token=ABKxQSuSEDMk9gCno2HB49mwZY9pyPezpLzI_0bgwXWQQ=&amp;xsec_source=pc_feed"
+          style="height: 297px;"><img data-v-e43676be=""
+                                      src="/images/5a75309011be1072dc4650c2/640d5d240000000012031b7e/1.png"
+                                      width="100" height="100"
+                                      style="width: 100%; height: 100%; object-fit: cover;"><!----><!----><!----></a>
+        <div data-v-e43676be="" className="footer"><!----><a data-v-e43676be="" target="_self" className="title"><span
+            data-v-7daa87a8="" data-v-e43676be="">长期骑行的第一批女受害者出现了</span></a>
+          <div data-v-e43676be="" className="author-wrapper"><a data-v-e43676be=""
+                                                                href="/user/profile/66b70869000000000b03011a?channel_type=explore_feed&amp;parent_page_channel_type=web_profile_board"
+                                                                className="author" target="_blank"><img
+              data-v-e43676be=""
+              src="https://sns-avatar-qc.xhscdn.com/avatar/645b80521e6a57c3bdb8f301.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip"
+              fetchpriority="auto"
+              width="20" height="20"
+              crossOrigin="anonymous"
+              loading="lazy"
+              decoding="async"
+              data-xhs-img=""
+              className="author-avatar"><span
+              data-v-e43676be="" className="name">Ce策策策策9621</span></a><span data-v-dc9968a0="" data-v-e43676be=""
+                                                                             className="like-wrapper like-active"><span
+              data-v-dc9968a0="" className="like-lottie" style="width: 16px; height: 16px;"></span><svg
+              data-v-23d27ada=""
+              data-v-dc9968a0=""
+              className="reds-icon like-icon"
+              width="16"
+              height="16"><use
+              data-v-23d27ada="" xlink:href="#like"></use></svg><span data-v-dc9968a0="" className="count"
+                                                                      selected-disabled-search="">58</span></span></div>
+        </div>
+      </div>
+    </section>
+    <section className="note-item" data-width="1083" data-height="1443" data-index="0">
+      <div data-v-e43676be=""><a data-v-e43676be="" href="/explore/66c32430000000001d0149d5" style="display: none;"></a><a
+          data-v-e43676be="" className="cover ld mask" target="_self"
+          href="/explore/66c32430000000001d0149d5?xsec_token=ABKxQSuSEDMk9gCno2HB49mwZY9pyPezpLzI_0bgwXWQQ=&amp;xsec_source=pc_feed"
+          style="height: 297px;"><img data-v-e43676be=""
+                                      src="/images/5a75309011be1072dc4650c2/640d5d240000000012031b7e/1.png"
+                                      width="100" height="100"
+                                      style="width: 100%; height: 100%; object-fit: cover;"><!----><!----><!----></a>
+        <div data-v-e43676be="" className="footer"><!----><a data-v-e43676be="" target="_self" className="title"><span
+            data-v-7daa87a8="" data-v-e43676be="">长期骑行的第一批女受害者出现了</span></a>
+          <div data-v-e43676be="" className="author-wrapper"><a data-v-e43676be=""
+                                                                href="/user/profile/66b70869000000000b03011a?channel_type=explore_feed&amp;parent_page_channel_type=web_profile_board"
+                                                                className="author" target="_blank"><img
+              data-v-e43676be=""
+              src="https://sns-avatar-qc.xhscdn.com/avatar/645b80521e6a57c3bdb8f301.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip"
+              fetchpriority="auto"
+              width="20" height="20"
+              crossOrigin="anonymous"
+              loading="lazy"
+              decoding="async"
+              data-xhs-img=""
+              className="author-avatar"><span
+              data-v-e43676be="" className="name">Ce策策策策9621</span></a><span data-v-dc9968a0="" data-v-e43676be=""
+                                                                             className="like-wrapper like-active"><span
+              data-v-dc9968a0="" className="like-lottie" style="width: 16px; height: 16px;"></span><svg
+              data-v-23d27ada=""
+              data-v-dc9968a0=""
+              className="reds-icon like-icon"
+              width="16"
+              height="16"><use
+              data-v-23d27ada="" xlink:href="#like"></use></svg><span data-v-dc9968a0="" className="count"
+                                                                      selected-disabled-search="">58</span></span></div>
+        </div>
+      </div>
+    </section>
+    <section className="note-item" data-width="1083" data-height="1443" data-index="0">
+      <div data-v-e43676be=""><a data-v-e43676be="" href="/explore/66c32430000000001d0149d5" style="display: none;"></a><a
+          data-v-e43676be="" className="cover ld mask" target="_self"
+          href="/explore/66c32430000000001d0149d5?xsec_token=ABKxQSuSEDMk9gCno2HB49mwZY9pyPezpLzI_0bgwXWQQ=&amp;xsec_source=pc_feed"
+          style="height: 297px;"><img data-v-e43676be=""
+                                      src="/images/5a75309011be1072dc4650c2/640d5d240000000012031b7e/1.png"
+                                      width="100" height="100"
+                                      style="width: 100%; height: 100%; object-fit: cover;"><!----><!----><!----></a>
+        <div data-v-e43676be="" className="footer"><!----><a data-v-e43676be="" target="_self" className="title"><span
+            data-v-7daa87a8="" data-v-e43676be="">长期骑行的第一批女受害者出现了</span></a>
+          <div data-v-e43676be="" className="author-wrapper"><a data-v-e43676be=""
+                                                                href="/user/profile/66b70869000000000b03011a?channel_type=explore_feed&amp;parent_page_channel_type=web_profile_board"
+                                                                className="author" target="_blank"><img
+              data-v-e43676be=""
+              src="https://sns-avatar-qc.xhscdn.com/avatar/645b80521e6a57c3bdb8f301.jpg?imageView2/2/w/60/format/webp|imageMogr2/strip"
+              fetchpriority="auto"
+              width="20" height="20"
+              crossOrigin="anonymous"
+              loading="lazy"
+              decoding="async"
+              data-xhs-img=""
+              className="author-avatar"><span
+              data-v-e43676be="" className="name">Ce策策策策9621</span></a><span data-v-dc9968a0="" data-v-e43676be=""
+                                                                             className="like-wrapper like-active"><span
+              data-v-dc9968a0="" className="like-lottie" style="width: 16px; height: 16px;"></span><svg
+              data-v-23d27ada=""
+              data-v-dc9968a0=""
+              className="reds-icon like-icon"
+              width="16"
+              height="16"><use
+              data-v-23d27ada="" xlink:href="#like"></use></svg><span data-v-dc9968a0="" className="count"
+                                                                      selected-disabled-search="">58</span></span></div>
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
@@ -25,14 +129,13 @@ export default {
 
   },
   methods: {},
-  computed: {
-  },
+  computed: {},
   created() {
     let timestamp = new Date().getTime();
     let start_timestamp = parseInt(timestamp) - 3600 * 24 * 7 * 1000;
     let end_timestamp = parseInt(timestamp);
     this.date = (this.$route.query.startTime == undefined || this.$route.query.endTime == undefined) ? [new Date(start_timestamp), new Date(end_timestamp)] : [new Date(this.$route.query.startTime), new Date(this.$route.query.endTime)];
-    this.getTableData()
+    // this.getTableData()
   },
   watch: {
     search() {
